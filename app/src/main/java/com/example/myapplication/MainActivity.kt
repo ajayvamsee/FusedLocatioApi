20b561a815c7f6b2b0f14013d03ebc84caa89e28
ajayvamsee24 <ajay.vamsee@techolution.com>
2021-12-29 13:07:02 +0530

    2nd commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,15 +1,27 @@
 package com.example.myapplication
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModel
+import androidx.core.app.ActivityCompat
 import com.example.myapplication.databinding.ActivityMainBinding
-import kotlinx.android.synthetic.main.activity_main.*
+import com.example.myapplication.utils.PermissionUtils
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 999
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,12 +29,88 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+    }
 
+    override fun onStart() {
+        super.onStart()
 
+        when {
+            PermissionUtils(this).isAccessFineLocationGranted(this)->{
+                when{
+                    PermissionUtils(this).isLocationEnabled(this)->{
+                        setUpLocationListener()
+                    }
+                    else->{
+                        PermissionUtils(this).showGPSNotEnabledDialog(this)
+                    }
+                }
+            }
+            else->{
+                PermissionUtils(this).requestAccessFineLocationPermission(this,
+                    LOCATION_PERMISSION_REQUEST_CODE)
+            }
+        }
+    }
 
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
 
+        when (requestCode) {
+            LOCATION_PERMISSION_REQUEST_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    when {
+                        PermissionUtils(this).isAccessFineLocationGranted(this) -> {
+                            setUpLocationListener()
+                        }
+                    }
+                }
+            }
+        }
+    }
 
-
+    private fun setUpLocationListener() {
+        val fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        // for getting the current location update after every 2 seconds with high accuracy
+        val locationRequest = LocationRequest().setInterval(2000).setFastestInterval(2000)
+            .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        Looper.myLooper()?.let {
+            fusedLocationProviderClient.requestLocationUpdates(
+                locationRequest,
+                object : LocationCallback() {
+                    override fun onLocationResult(locationResult: LocationResult) {
+                        super.onLocationResult(locationResult)
+                        for (location in locationResult.locations) {
+                            binding.latTextView?.text = location.latitude.toString()
+                            binding.lngTextView?.text = location.longitude.toString()
+                        }
+                        // Few more things we can do here:
+                        // For example: Update the location of user on server
+                    }
+                },
+                it
+            )
+        }
     }
 }
 
--- app/src/main/java/com/example/myapplication/utils/PermissionUtils.kt
+++ app/src/main/java/com/example/myapplication/utils/PermissionUtils.kt
@@ -12,44 +12,54 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.myapplication.R
 
-class PermissionUtils {
+class PermissionUtils(context: Context) {
 
     /**
      * Function to request permission from the user
      */
 
-    fun requestAccessFineLocationPermission(activity:AppCompatActivity,requestId:Int){
-        ActivityCompat.requestPermissions(activity, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),requestId)
+    fun requestAccessFineLocationPermission(activity: AppCompatActivity, requestId: Int) {
+        ActivityCompat.requestPermissions(
+            activity,
+            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+            requestId
+        )
     }
 
     /**
      * Function to check if the location permission are granted or not
      */
 
-    fun isAccessFineLocationGranted(context: Context):Boolean{
-        return ContextCompat.checkSelfPermission(context,Manifest.permission.ACCESS_FINE_LOCATION )==PackageManager.PERMISSION_GRANTED
+    fun isAccessFineLocationGranted(context: Context): Boolean {
+        return ContextCompat.checkSelfPermission(
+            context,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
     }
 
     /**
      * Function to check the if location of the device is enabled or not
      */
 
-    fun isLocationEnabled(context: Context):Boolean{
-        val locationManager:LocationManager=context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+    fun isLocationEnabled(context: Context): Boolean {
+        val locationManager: LocationManager =
+            context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
 
-        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)||locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER
+        )
     }
 
     /**
      * Function to show the "enable GPS" Dialog box
      */
 
-    fun showGPSNotEnabledDialog(context: Context){
+    fun showGPSNotEnabledDialog(context: Context) {
         AlertDialog.Builder(context)
             .setTitle(context.getString(R.string.enable_gps))
             .setMessage(context.getString(R.string.required_for_this_app))
             .setCancelable(false)
-            .setPositiveButton(context.getString(R.string.enable_now)){ _, _ ->
+            .setPositiveButton(context.getString(R.string.enable_now)) { _, _ ->
                 context.startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS))
             }
             .show()
